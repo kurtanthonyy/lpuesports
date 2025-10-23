823830d234fbc8e2fb8fcc620a191b10ebec6c57
kurtanthonyy <qkamggrinio@tip.edu.ph>
2025-10-24 01:35:48 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add background to header on scroll
+const header = document.getElementById("main-header");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+});
